8f13156a04eabad232d01fe9e77ea0dfc1c37b09
Adver-sari <147937765+Adver-sari@users.noreply.github.com>
2023-10-14 20:01:10 +0300

    Update imagenet.js

--- docs/js/imagenet.js
+++ docs/js/imagenet.js
@@ -42,6 +42,7 @@ let xUrls = [
   'data/imagenet/0_tench.jpg',
   'data/imagenet/497_church.jpg',
   'data/imagenet/566_french_horn.jpg'
+  'data/imagenet/150425091440566638_8.jpg'
 ]
 let yLbls = [574, 217, 701, 0, 497, 566]
 let y = yLbls.map(lbl => tf.oneHot(lbl, 1000).reshape([1, 1000]));
